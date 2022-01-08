8951fd7c58bb033af5e4b42ae1e1b617df0612ac
Albert Ramos <albertramos.dev@gmail.com>
2022-01-07 20:08:19 -0700

    Add file

---
+++ polish_problem.py
@@ -0,0 +1,16 @@
+a = input('Input a number ')
+print(type(a))
+x = input('Input a second number')
+print(type(x))
+
+int(x)
+print(type(x))
+
+int(a)
+print(type(a))
+
+h = x // 24
+s = x % 24
+print(h, s)
+a = a + s
+print('godzina teraz', a)
