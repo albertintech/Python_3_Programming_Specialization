4a74895b91a02871d62057997738978d5d420891
Albert Ramos <albertramos.dev@gmail.com>
2022-01-17 06:18:00 -0700

    Add assessment folder

---
+++ Course_1/week3/assessment/8.14.1.py
@@ -0,0 +1,3 @@
+"""rainfall_mi is a string that contains the average number of inches of rainfall in Michigan for every month (in inches) with every month separated by a comma. Write code to compute the number of months that have more than 3 inches of rainfall. Store the result in the variable num_rainy_months. In other words, count the number of items with values > 3.0.
+
+Hard-coded answers will receive no credit."""
