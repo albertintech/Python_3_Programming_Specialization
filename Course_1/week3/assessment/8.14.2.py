fc6fd5462a3fd48d3da17ff2450afa94d4977781
Albert Ramos <albertramos.dev@gmail.com>
2022-01-17 06:21:15 -0700

    Add questions

---
+++ Course_1/week3/assessment/8.14.2.py
@@ -0,0 +1,3 @@
+"""The variable sentence stores a string. Write code to determine how many words in sentence start and end with the same letter, including one-letter words. Store the result in the variable same_letter_count.
+
+Hard-coded answers will receive no credit."""
