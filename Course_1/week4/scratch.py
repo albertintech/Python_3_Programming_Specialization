08d8220a0b78c30ad5031aa0668575f2e81890f2
Albert Ramos <albertramos.dev@gmail.com>
2022-01-17 16:57:41 -0700

    Update file

--- Course_1/week4/scratch.py
+++ Course_1/week4/scratch.py
@@ -1,17 +1,51 @@
-alist = ['a', 'd', 'f']
-print(alist)
-alist[1:1] = ['b', 'c']
-print(alist)
-alist[4:4] = ['e']
-print(alist)
+# alist = ['a', 'd', 'f']
+# print(alist)
+# alist[1:1] = ['b', 'c']
+# print(alist)
+# alist[4:4] = ['e']
+# print(alist)
 
-phrase = "many moons"
-print(phrase)
-phrase_expanded = phrase + " and many stars"
-print(phrase_expanded)
-phrase_larger = phrase_expanded + " litter"
-print(phrase_larger)
-phrase_complete = "M" + phrase_larger[1:] + " the night sky."
-print(phrase_complete)
-excited_phrase_complete = phrase_complete[:-1] + "!"
-print(excited_phrase_complete)
+# phrase = "many moons"
+# print(phrase)
+# phrase_expanded = phrase + " and many stars"
+# print(phrase_expanded)
+# phrase_larger = phrase_expanded + " litter"
+# print(phrase_larger)
+# phrase_complete = "M" + phrase_larger[1:] + " the night sky."
+# print(phrase_complete)
+# excited_phrase_complete = phrase_complete[:-1] + "!"
+# print(excited_phrase_complete)
+
+# a = ['one', 'two', 'three']
+# del a[1]
+# print(a)
+
+# alist = ['a', 'b', 'c', 'd', 'e', 'f']
+# del alist[1:5]
+# print(alist)
+
+# a = "banana"
+# b = "banana"
+
+# print(a is b)
+
+# a = "banana"
+# b = "banana"
+
+# print(id(a))
+# print(id(b))
+
+# a = [81, 82, 83]
+# b = [81, 82, 83]
+
+# print(a is b)
+
+# print(a == b)
+
+# print(id(a))
+# print(id(b))
+
+alist = [4, 2, 8, 6, 5]
+blist = alist * 2
+blist[3] = 999
+print(alist)
