6b4da64eb3d17c37aeec79267e93d4fae39a95de
Albert Ramos <albertramos.dev@gmail.com>
2022-01-17 06:34:40 -0700

    Add solution

--- Course_1/week3/assessment/8.14.4.py
+++ Course_1/week3/assessment/8.14.4.py
@@ -7,3 +7,13 @@ HINT 1: Use the in operator.
 HINT 2: You can either use or or elif.
 
 Hard-coded answers will receive no credit."""
+
+sentence = "python is a high level general purpose programming language that can be applied to many different classes of problems."
+
+words = sentence.split()
+
+num_a_or_e = 0
+for word in words:
+    if 'a' in word or 'e' in word:
+        num_a_or_e += 1
+print("The number of words with either 'a' or 'e' is:", num_a_or_e)
