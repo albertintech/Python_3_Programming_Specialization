7727ebc5d98852418f69077dbde3b6eaa87ef29a
Albert Ramos <albertramos.dev@gmail.com>
2022-01-19 18:24:01 -0700

    Update file

--- Course_1/week4/scratch.py
+++ Course_1/week4/scratch.py
@@ -1,19 +1,47 @@
-st = "Warmth"
+# nums = [3, 5, 8]
+# accum = []
+# for w in nums:
+#     x = w**2
+#     accum.append(x)
+# print(accum)  # [9, 25, 64]
+# print(nums)
 
-st_lst = list(st)
+# nums = [2, 4, 9]
+# accum = []
+# for w in nums:
+#     x = w**2
+#     accum += [x]
+# print(accum)  # [4, 16, 81]
+# print(nums)
 
-print(st_lst)
+# verbs = ["kayak", "cry", "walk", "eat", "drink", "fly"]
 
-s = st.replace('th', '')
-print(s)
+# ing = []
 
-t = "   twisters "
-print(t)
-u = t.strip()
-print(u)
+# for verb in verbs:
+#     ing += [verb + 'ing']
+# print(ing)
 
-v = 'velociraptor'
-print(v.index('r'))
+# numbs = [5, 10, 15, 20, 25]
+# print(numbs)
+# newlist = []
 
-person = input('Enter your name: ')
-print('Hello {}!'.format(person))
+# for n in numbs:
+#     i = n + 5
+#     newlist.append(i)
+# print(newlist)
+
+# numbs = [5, 10, 15, 20, 25]
+# print(numbs)
+# count = 0
+# for n in numbs:
+#     numbs[count] = n + 5
+#     count += 1
+# print(numbs)
+
+lst_nums = [4, 29, 5.3, 10, 2, 1817, 1967, 9, 31.32]
+larger_nums = []
+for n in lst_nums:
+    x = n*2
+    larger_nums.append(x)
+print(larger_nums)
