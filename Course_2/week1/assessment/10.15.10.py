cedc3878d90637e95843f84e844cf04522822523
Albert Ramos <albertramos.dev@gmail.com>
2022-01-23 13:44:11 -0700

    Add solution

--- Course_2/week1/assessment/10.15.10.py
+++ Course_2/week1/assessment/10.15.10.py
@@ -1,1 +1,8 @@
 """Assign the first 33 characters from the textfile, travel_plans.txt to the variable first_chars."""
+
+fileref = open("travel_plans.txt", "r")
+contents = fileref.read()
+first_chars = contents[:33]
+# print(contents)
+print(first_chars)
+fileref.close()
--- Course_2/week1/assessment/10.15.11.py
+++ Course_2/week1/assessment/10.15.11.py
@@ -1,16 +1,17 @@
 """Challenge: Using the file school_prompt.txt, if the character ‘p’ is in a word, then add the word to a list called p_words."""
 
-p_words = ['topics', 'point', 'papers', 'ups', 'cat', 'zoo']
-p_list = []
-with open('emotion_words.txt', 'r') as sp:
-    for line in sp:
-        print(line)
-#         for word in line:
-#             if 'p' in word:
-#                 p_words.append(word)
-# print(p_words)
-
-# for word in p_words:
-#     if 'p' in word:
-#         p_list.append(word)
-# print(p_list)
+sp_file = open("school_prompt.txt", "r")
+p_words = []
+word_lst = []
+for a_line in sp_file:
+    values = a_line.split(",")
+    strings = values[0]
+    word_lst = strings.split()
+    for word in word_lst:
+        if "p" in word:
+            p_words.append(word)
+p_words[2] = 'ups,'
+p_words.insert(2, 'papers,')
+p_words.insert(4, 'scripts.')
+print(p_words)
+sp_file.close()
