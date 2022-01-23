4a08b3d9bc47c07cc96761f1a032da626c9b5157
Albert Ramos <albertramos.dev@gmail.com>
2022-01-23 12:41:08 -0700

    Add solution

--- Course_2/week1/assessment/10.15.5.py
+++ Course_2/week1/assessment/10.15.5.py
@@ -1,1 +1,8 @@
 """We have provided a file called emotion_words.txt that contains lines of words that describe emotions. Find the total number of words in the file and assign this value to the variable num_words."""
+
+fileref = open("emotion_words.txt", "r")
+contents = fileref.read()
+word_lst = []
+word_lst = contents.split()
+print(len(word_lst))
+fileref.close()
