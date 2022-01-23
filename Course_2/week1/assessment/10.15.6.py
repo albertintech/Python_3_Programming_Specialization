712fd95c305f4555564703be5b6267a2bc5df51b
Albert Ramos <albertramos.dev@gmail.com>
2022-01-23 13:23:25 -0700

    Add solution

--- Course_2/week1/assessment/10.15.6.py
+++ Course_2/week1/assessment/10.15.6.py
@@ -1,1 +1,6 @@
 """Assign to the variable num_lines the number of lines in the file school_prompt.txt."""
+
+fileref = open("school_prompt.txt", "r")
+lines = fileref.readlines()
+num_lines = len(lines)
+fileref.close()
--- Course_2/week1/assessment/10.15.7.py
+++ Course_2/week1/assessment/10.15.7.py
@@ -1,1 +1,7 @@
 '''Assign the first 30 characters of school_prompt.txt as a string to the variable beginning_chars.'''
+
+fileref = open("school_prompt.txt", "r")
+contents = fileref.read()
+beginning_chars = contents[:30]
+print(beginning_chars)
+fileref.close()
