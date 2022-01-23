7213c386c08cf4870a33239da1efa8cfc10c27bb
Albert Ramos <albertramos.dev@gmail.com>
2022-01-23 12:31:14 -0700

    Add solution

--- Course_2/week1/assessment/10.15.4.py
+++ Course_2/week1/assessment/10.15.4.py
@@ -1,1 +1,7 @@
 """The textfile, travel_plans.txt, contains the summer travel plans for someone with some commentary. Find the total number of characters in the file and save to the variable num."""
+
+fileref = open("travel_plans.txt", "r")
+contents = fileref.read()
+num = len(contents)
+print(num)
+fileref.close()
