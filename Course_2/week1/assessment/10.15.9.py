d1cdbd3c4ab121f828dc98333d1f295283c3bd85
Albert Ramos <albertramos.dev@gmail.com>
2022-01-23 13:27:17 -0700

    Add solution

--- Course_2/week1/assessment/10.15.9.py
+++ Course_2/week1/assessment/10.15.9.py
@@ -1,1 +1,11 @@
 '''Challenge: Create a list called emotions that contains the first word of every line in emotion_words.txt.'''
+
+ew_file = open("emotion_words.txt", "r")
+emotions = []
+for a_line in ew_file:
+    values = a_line.split(",")
+    strings = values[0]
+    emotions.append(strings.split()[0])
+
+print(emotions)
+ew_file.close()
