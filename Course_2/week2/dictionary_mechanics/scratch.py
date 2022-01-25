82f2e30f3f2d6fe1860e62b0854c33d0df9f5ef3
Albert Ramos <albertramos.dev@gmail.com>
2022-01-25 07:27:33 -0700

    Update file

--- Course_2/week2/dictionary_mechanics/scratch.py
+++ Course_2/week2/dictionary_mechanics/scratch.py
@@ -5,3 +5,15 @@ eng2sp['two'] = 'dos'
 eng2sp['three'] = 'tres'
 
 print(eng2sp)
+
+# Alternatively:
+eng2sp = {'three': 'tres', 'one': 'uno', 'two': 'dos'}
+
+eng2rus = {}
+eng2rus['one'] = 'adin'
+eng2rus['two'] = 'dva'
+eng2rus['three'] = 'trri'
+
+print(eng2rus)
+
+# It doesn’t matter what order we write the pairs. The values in a dictionary are accessed with keys, not with indices, so there is no need to care about ordering.
