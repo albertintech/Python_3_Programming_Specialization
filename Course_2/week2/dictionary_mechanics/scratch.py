c20700b155c2f6248bf5a0af2e0403baba5cf8b5
Albert Ramos <albertramos.dev@gmail.com>
2022-01-25 19:19:17 -0700

    Update file

--- Course_2/week2/dictionary_mechanics/scratch.py
+++ Course_2/week2/dictionary_mechanics/scratch.py
@@ -9,6 +9,10 @@ print(eng2sp)
 # Alternatively:
 eng2sp = {'three': 'tres', 'one': 'uno', 'two': 'dos'}
 
+value = eng2sp['two']
+print(value)
+print(eng2sp['one'])
+
 eng2rus = {}
 eng2rus['one'] = 'adin'
 eng2rus['two'] = 'dva'
@@ -17,3 +21,9 @@ eng2rus['three'] = 'trri'
 print(eng2rus)
 
 # It doesn’t matter what order we write the pairs. The values in a dictionary are accessed with keys, not with indices, so there is no need to care about ordering.
+
+inventory = {'apples': 430, 'bananas': 312, 'oranges': 525, 'pears': 217}
+inventory['bananas'] += 200
+print(inventory)
+numItems = len(inventory)
+print(numItems)
