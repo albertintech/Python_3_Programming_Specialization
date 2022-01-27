24d877d211b870ee3062c70ea0378a61a205db3d
Albert Ramos <albertramos.dev@gmail.com>
2022-01-27 07:15:51 -0700

    Update file

--- Course_2/week2/dictionary_accumulation/scratch.py
+++ Course_2/week2/dictionary_accumulation/scratch.py
@@ -1,1 +1,16 @@
 """Dictionary Accumulation"""
+
+f = open('lorem_ipsum.txt', 'r')
+txt = f.read()
+# txt is now a string of entire char set of lorem_ipsum.txt
+t_count = 0  # initialize the accumlator variable
+s_count = 0
+for c in txt:
+    if c == 't':
+        t_count += 1  # increment the counter
+    elif c == 's':
+        s_count += 1
+print("t: " + str(t_count) + " occurences")
+print("s: " + str(s_count) + " occurences")
+
+# Now, let's use a dictionary!
