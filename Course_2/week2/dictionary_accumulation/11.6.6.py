45cbc1d9ea4e018d5d4cd08a82f70a260b9c151a
Albert Ramos <albertramos.dev@gmail.com>
2022-01-29 09:39:17 -0700

    Add key accumulationfor loop

--- Course_2/week2/dictionary_accumulation/11.6.6.py
+++ Course_2/week2/dictionary_accumulation/11.6.6.py
@@ -8,6 +8,14 @@ for c in txt:
         letter_counts[c] = 0
     letter_counts[c] += 1
 
-print("t: " + str(letter_counts['t']) + " occurrences")
-print("s: " + str(letter_counts['s']) + " occurrences")
-print("' ': " + str(letter_counts[' ']) + " occurrences")
+# print("t: " + str(letter_counts['t']) + " occurrences")
+# print("e: " + str(letter_counts['s']) + " occurrences")
+# print("' ': " + str(letter_counts[' ']) + " occurrences")
+
+# A Clever Alternative:
+# Replace the above for loop with these two lines:
+# for c in txt:
+#    letter_counts[c] = letter_counts.get(c, 0) + 1
+
+for c in letter_counts.keys():
+    print(c + ": " + str(letter_counts[c]) + " occurrences")
