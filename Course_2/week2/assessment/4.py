2e0bbb25a8ff991dffd9fc18281feaec8ed6655a
Albert Ramos <albertramos.dev@gmail.com>
2022-01-29 19:59:30 -0700

    Add solution

--- Course_2/week2/assessment/4.py
+++ Course_2/week2/assessment/4.py
@@ -1,1 +1,8 @@
 str1 = "I wish I wish with all my heart to fly with dragons in a land apart"
+lst = str1.split()
+freq_words = {}
+for word in lst:
+    if word not in freq_words:
+        freq_words[word] = 0
+    freq_words[word] += 1
+print(freq_words)
