300c382a101b023011402696400efffe34ce22cf
Albert Ramos <albertramos.dev@gmail.com>
2022-01-29 20:26:37 -0700

    Add solution

--- Course_2/week2/assessment/6.py
+++ Course_2/week2/assessment/6.py
@@ -1,1 +1,16 @@
 sally = "sally sells sea shells by the sea shore"
+
+characters = {}
+best_char = 0
+for c in sally:
+    if c not in characters:
+        characters[c] = 0
+    characters[c] += 1
+
+keys = list(characters.keys())
+best_char = keys[0]
+
+for key in keys:
+    if characters[key] > characters[best_char]:
+        best_char = key
+print(best_char)
