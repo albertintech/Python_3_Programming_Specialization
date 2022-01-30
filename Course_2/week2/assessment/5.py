3635e87f50bad33232fe73a90c281ee0961eaa5c
Albert Ramos <albertramos.dev@gmail.com>
2022-01-29 20:26:15 -0700

    Add solution

--- Course_2/week2/assessment/5.py
+++ Course_2/week2/assessment/5.py
@@ -1,1 +1,8 @@
 sent = "Singing in the rain and playing in the rain are two entirely different situations but both can be good"
+lst = sent.split()
+wrd_d = {}
+for word in lst:
+    if word not in wrd_d:
+        wrd_d[word] = 0
+    wrd_d[word] += 1
+print(wrd_d)
