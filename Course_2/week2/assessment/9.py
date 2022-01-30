c22dbd305853fb6f5d08bf7c9a019a11ce21c052
Albert Ramos <albertramos.dev@gmail.com>
2022-01-29 21:36:22 -0700

    Add solution

--- Course_2/week2/assessment/9.py
+++ Course_2/week2/assessment/9.py
@@ -1,1 +1,9 @@
 p = "Summer is a great time to go outside. You have to be careful of the sun though because of the heat."
+p_low = p.lower()
+
+low_d = {}
+for c in p_low:
+    if c not in low_d:
+        low_d[c] = 0
+    low_d[c] += 1
+print(low_d)
