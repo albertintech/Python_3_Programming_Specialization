2b1030a55bed6241605e5bfc78bed5a6228e3cb1
Albert Ramos <albertramos.dev@gmail.com>
2022-01-29 20:45:50 -0700

    Add solution

--- Course_2/week2/assessment/7.py
+++ Course_2/week2/assessment/7.py
@@ -1,1 +1,16 @@
 sally = "sally sells sea shells by the sea shore and by the road"
+
+characters = {}
+worst_char = 0
+for c in sally:
+    if c not in characters:
+        characters[c] = 0
+    characters[c] += 1
+print(characters)
+keys = list(characters.keys())
+worst_char = keys[0]
+
+for key in keys:
+    if characters[key] < characters[worst_char]:
+        worst_char = key
+print(worst_char)
