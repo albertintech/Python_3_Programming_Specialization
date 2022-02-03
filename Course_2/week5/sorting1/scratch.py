9298c18e45474b1cdaa7d1513b45b568641d69e3
Albert Ramos <albertramos.dev@gmail.com>
2022-02-03 06:47:43 -0700

    Add sorted with reverse parameter

--- Course_2/week5/sorting1/scratch.py
+++ Course_2/week5/sorting1/scratch.py
@@ -1,11 +1,12 @@
 """Sorting Basics"""
-# l1 = [1, 7, 4, -2, 3]
-# l2 = ["Cherry", "Apple", "Blueberry"]
+l1 = [1, 7, 4, -2, 3]
+l2 = ["Cherry", "Apple", "Blueberry"]
 
-# l1.sort()
-# print(l1)
-# l2.sort()
-# print(l2)
+l1.sort()
+print(l1)
+l2.sort()
+print(l2)
+
 
 l2 = ["Cherry", "Apple", "Blueberry"]
 print("Original: ", l2)
@@ -18,4 +19,11 @@ print("Original: ", l2)
 print("Has object id: ", id(l2))
 
 print(sorted("Apple"))  # returns a list of chars in alpha order
-"Apple".sort()  # will fail, only works on list
+# "Apple".sort()  # will fail, only works on list
+
+l2 = ["Cherry", "Apple", "Blueberry"]
+print(sorted(l2, reverse=True))  # reverse defaults to False
+
+lst = [3, 5, 1, 6, 7, 2, 9, -2, 5]
+lst_sorted = sorted(lst, reverse=True)
+print(lst_sorted)
