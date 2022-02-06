9668fbdadc7e7035a1d7ac2a5e9509b2cc01687e
Albert Ramos <albertramos.dev@gmail.com>
2022-02-06 06:49:54 -0700

    Add question

---
+++ Course_2/week5/assessment/10.py
@@ -0,0 +1,3 @@
+# Sort the list ids by the last four digits of each id. Do this using lambda and not using a defined function. Save this sorted list in the variable sorted_id.
+
+ids = [17573005, 17572342, 17579000, 17570002, 17572345, 17579329]
