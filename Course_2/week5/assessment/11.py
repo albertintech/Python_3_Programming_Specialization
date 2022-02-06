e59ac94eac3d1196a386a4db4e2587ecf077c35c
Albert Ramos <albertramos.dev@gmail.com>
2022-02-06 11:10:13 -0700

    Add solution

--- Course_2/week5/assessment/11.py
+++ Course_2/week5/assessment/11.py
@@ -1,3 +1,7 @@
 # Sort the following list by each element’s second letter a to z. Do so by using lambda. Assign the resulting value to the variable lambda_sort.
 
 ex_lst = ['hi', 'how are you', 'bye', 'apple', 'zebra', 'dance']
+
+lambda_sort = sorted(ex_lst, key=lambda word: word[1])
+
+print(lambda_sort)
