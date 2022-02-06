5602f6f977d8d261de060915b5863e8618a6f31d
Albert Ramos <albertramos.dev@gmail.com>
2022-02-06 06:58:27 -0700

    Add question

---
+++ Course_2/week5/assessment/7.py
@@ -0,0 +1,4 @@
+# Given the same dictionary, medals, now sort by the medal count. Save the three countries with the highest medal count to the list, top_three.
+
+medals = {'Japan': 41, 'Russia': 56, 'South Korea': 21,
+          'United States': 121, 'Germany': 42, 'China': 70}
