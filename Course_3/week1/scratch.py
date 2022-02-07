2c404e9afe692f3ea556c297d067896c9b24f8eb
Albert Ramos <albertramos.dev@gmail.com>
2022-02-07 07:48:49 -0700

    Add nested example

--- Course_3/week1/scratch.py
+++ Course_3/week1/scratch.py
@@ -5,3 +5,22 @@ print(y[0])
 
 print([10, 20, 30][1])
 print(nested1[1][0])
+
+
+def square(x):
+    return x*x
+
+
+L = [square, abs, lambda x: x+1]
+
+print("****names****")
+for f in L:
+    print(f)
+
+print("****call each of them****")
+for f in L:
+    print(f(-2))
+
+print("****just the first one in the list****")
+print(L[0])
+print(L[0](3))
