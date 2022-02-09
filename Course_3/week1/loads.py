729e9a0005605253e2ac09ef0d6f04bd91461ef8
Albert Ramos <albertramos.dev@gmail.com>
2022-02-08 20:07:23 -0700

    Add loads.py

---
+++ Course_3/week1/loads.py
@@ -0,0 +1,11 @@
+"""17.3. Processing JSON results"""
+import json
+
+a_string = '\n\n\n{\n "resultCount":25,\n "results": [\n{"wrapperType":"track", "kind":"podcast", "collectionId":10892}]}'
+print(a_string)
+d = json.loads(a_string)
+print("------")
+print(type(d))
+print(d.keys())
+print(d['resultCount'])
+# print(a_string['resultCount'])
