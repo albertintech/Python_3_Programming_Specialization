8ccc5e93dd55c6b17716d77de1b4d22ad5c39efe
Albert Ramos <albertramos.dev@gmail.com>
2022-02-09 21:13:49 -0700

    Update file

--- Course_3/week1/nested_iteration.py
+++ Course_3/week1/nested_iteration.py
@@ -1,24 +1,27 @@
-# nested1 = [['a', 'b', 'c'], ['d', 'e'], ['f', 'g', 'h']]
+nested1 = [['a', 'b', 'c'], ['d', 'e'], ['f', 'g', 'h']]
 
-# for x in nested1:
-#     print("level1: ")
-#     for y in x:
-#         print("     level2: " + y)
+for x in nested1:
+    print("level1: ")
+    for y in x:
+        print("     level2: " + y)
 
-# info = [['Tina', 'Turner', 1939, 'singer'], ['Matt', 'Damon', 1970, 'actor'], ['Kristen', 'Wiig',
-#                                                                                1973, 'comedian'], ['Michael', 'Phelps', 1985, 'swimmer'], ['Barack', 'Obama', 1961, 'president']]
-
-# # last_names = []
-# # for lst in info:
-# #     for x in lst:
-# #         if x == lst[1]:
-# #             last_names.append(x)
-# # print(last_names)
+info = [['Tina', 'Turner', 1939, 'singer'],
+        ['Matt', 'Damon', 1970, 'actor'],
+        ['Kristen', 'Wiig', 1973, 'comedian'],
+        ['Michael', 'Phelps', 1985, 'swimmer'],
+        ['Barack', 'Obama', 1961, 'president']]
 
 # last_names = []
 # for lst in info:
-#     last_names.append(lst[1])
+#     for x in lst:
+#         if x == lst[1]:
+#             last_names.append(x)
 # print(last_names)
+
+last_names = []
+for lst in info:
+    last_names.append(lst[1])
+print(last_names)
 
 L = [['apples', 'bananas', 'oranges', 'blueberries', 'lemons'],
      ['carrots', 'peas', 'cucumbers', 'green beans'],
