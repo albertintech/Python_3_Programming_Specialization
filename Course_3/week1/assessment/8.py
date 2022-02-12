3b9bdbe4b91ca41655d21c2267c771a04945cf87
Albert Ramos <albertramos.dev@gmail.com>
2022-02-12 15:52:48 -0700

    Add question

---
+++ Course_3/week1/assessment/8.py
@@ -0,0 +1,4 @@
+# Iterate through the contents of l_of_l and assign the third element of sublist to a new list called third.
+
+l_of_l = [['purple', 'mauve', 'blue'], ['red', 'maroon', 'blood orange', 'crimson'], [
+    'sea green', 'cornflower', 'lavender', 'indigo'], ['yellow', 'amarillo', 'mac n cheese', 'golden rod']]
