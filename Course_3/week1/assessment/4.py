cb577ca527d08ae73c0a7aa13d2b2b7a14f82836
Albert Ramos <albertramos.dev@gmail.com>
2022-02-12 15:32:40 -0700

    Add solution

--- Course_3/week1/assessment/4.py
+++ Course_3/week1/assessment/4.py
@@ -1,12 +1,27 @@
 # Provided is a nested data structure. Follow the instructions in the comments below. Do not hard code.
 
-nested = {'data': ['finding', 23, ['exercises', 'hangout', 34]], 'window': ['part', 'whole', [], 'sum', [
-    'math', 'calculus', 'algebra', 'geometry', 'statistics', ['physics', 'chemistry', 'biology']]]}
+nested = {'data': ['finding', 23, ['exercises', 'hangout', 34]],
+          'window': ['part', 'whole', [], 'sum', ['math', 'calculus', 'algebra', 'geometry', 'statistics', ['physics', 'chemistry', 'biology']]]}
 
 # Check to see if the string 'data' is a key in nested, if it is, assign True to the variable data, otherwise assign False.
-
+data = False
+if 'data' in nested.keys():
+    data = True
+print(data)
 # Check to see if the integer 24 is in the value of the key data, if it is then assign to the variable twentyfour the value of True, otherwise False.
-
+twentyfour = False
+for item in nested['data']:
+    if item == 24:
+        twentyfour = True
+print(twentyfour)
 # Check to see that the string 'whole' is not in the value of the key window. If it's not, then assign to the variable whole the value of True, otherwise False.
-
+whole = True
+for item in nested['window']:
+    if item == 'whole':
+        whole = False
+print(whole)
 # Check to see if the string 'physics' is a key in the dictionary nested. If it is, assign to the variable physics, the value of True, otherwise False.
+physics = False
+if 'physics' in nested.keys():
+    physics = True
+print(physics)
