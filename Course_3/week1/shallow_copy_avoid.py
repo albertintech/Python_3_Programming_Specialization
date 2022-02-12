d9d6b2f60cd3b8c950e815e9223bcdedf3da77a1
Albert Ramos <albertramos.dev@gmail.com>
2022-02-11 18:08:40 -0700

    Avoid shallow copy with for loops

---
+++ Course_3/week1/shallow_copy_avoid.py
@@ -0,0 +1,14 @@
+"""Assuming that you don’t want to have aliased lists inside of your nested list, then you’ll need to perform nested iteration."""
+
+original = [['dogs', 'puppies'], ['cats', "kittens"]]
+copied_outer_list = []
+for inner_list in original:
+    copied_inner_list = []
+    for item in inner_list:
+        copied_inner_list.append(item)
+    copied_outer_list.append(copied_inner_list)
+print(copied_outer_list)
+original[0].append(["canines"])
+print(original)
+print("-------- Now look at the copied version -----------")
+print(copied_outer_list)
