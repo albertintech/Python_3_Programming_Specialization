d31a1c0d0043da5cdf9e13cb8a4645cf518fcb5d
Albert Ramos <albertramos.dev@gmail.com>
2022-02-16 05:54:35 -0700

    Add problem

---
+++ Course_3/week2/assessment/6.py
@@ -0,0 +1,4 @@
+# Write code using zip and filter so that these lists (l1 and l2) are combined into one big list and assigned to the variable opposites if they are both longer than 3 characters each.
+
+l1 = ['left', 'up', 'front']
+l2 = ['right', 'down', 'back']
