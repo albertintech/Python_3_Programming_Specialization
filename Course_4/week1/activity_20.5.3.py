7506d48c87062594394c86cf46e4b7ea0f60e9d4
Albert Ramos <albertramos.dev@gmail.com>
2022-02-18 03:49:08 -0700

    Add solution

--- Course_4/week1/activity_20.5.3.py
+++ Course_4/week1/activity_20.5.3.py
@@ -1,1 +1,22 @@
 # Create a class called Animal that accepts two numbers as inputs and assigns them respectively to two instance variables: arms and legs. Create an instance method called limbs that, when called, returns the total number of limbs the animal has. To the variable name spider, assign an instance of Animal that has 4 arms and 4 legs. Call the limbs method on the spider instance and save the result to the variable name spidlimbs.
+
+class Animal:
+
+    def __init__(self, initX, initY):
+
+        self.arms = initX
+        self.legs = initY
+
+    def getArms(self):
+        return self.arms
+
+    def getLegs(self):
+        return self.legs
+
+    def limbs(self):
+        return self.arms + self.legs
+
+
+spider = Animal(4, 4)
+spidlimbs = spider.limbs()
+print(spidlimbs)
