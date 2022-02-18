fa0280516731bff3130ef041ef2d0f627b922d4b
Albert Ramos <albertramos.dev@gmail.com>
2022-02-18 04:26:21 -0700

    Add __str__ comment

--- Course_4/week1/other_methods.py
+++ Course_4/week1/other_methods.py
@@ -25,3 +25,11 @@ p = Point(7, 6)
 print(p)
 p2 = Point(8, 9)
 print(p2)
+
+"""The default functionality provided by Python tells you that p is an object of type Point. However, it does not tell you anything about the specific state of the point.
+
+We can improve on this representation if we include a special method call __str__. Notice that this method uses the same naming convention as the constructor, that is two underscores before and after the name. It is common that Python uses this naming technique for special methods.
+
+The __str__ method is responsible for returning a string representation as defined by the class creator. In other words, you as the programmer, get to choose what a Point should look like when it gets printed. In this case, we have decided that the string representation will include the values of x and y as well as some identifying text. It is required that the __str__ method create and return a string.
+
+Whatever string the __str__ method for a class returns, that is the string that will print when you put any instance of that class in a print statement. For that reason, the string that a class’s __str__ method returns should usually include values of instance variables. If a point has x value 3 and y value 4, but another point has x value 5 and y value 9, those two Point objects should probably look different when you print them, right?"""
