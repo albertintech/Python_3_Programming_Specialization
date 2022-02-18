16080206a1a11afb412f4e2f971355509039ddca
Albert Ramos <albertramos.dev@gmail.com>
2022-02-18 04:10:38 -0700

    Add example file

---
+++ Course_4/week1/class_data.py
@@ -0,0 +1,24 @@
+from os import stat
+
+
+cityNames = ['Detroit', 'Ann Arbor', 'Pittsburgh', 'Mars', 'New York']
+pop = [680250, 117070, 304391, 1683, 8406000]
+states = ['MI', 'MI', 'PA', 'PA', 'NY']
+
+city_tuples = list(zip(cityNames, pop, states))
+# print(city_tuples)
+
+
+class City:
+
+    def __init__(self, n, p, s):
+        self.name = n
+        self.pop = p
+        self.state = s
+
+    def __str__(self):
+        return '{}, {} (pop: {})'.format(self.name, self.state, self.pop)
+
+
+cities = [City(n, p, s) for (n, p, s) in city_tuples]
+print(cities)
