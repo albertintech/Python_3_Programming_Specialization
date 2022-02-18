20fdeebbeb04bf90ae1f44264b90bca621c3ea65
Albert Ramos <albertramos.dev@gmail.com>
2022-02-18 04:20:42 -0700

    Add __str__ method

--- Course_4/week1/other_methods.py
+++ Course_4/week1/other_methods.py
@@ -14,8 +14,14 @@ class Point:
     def distanceFromOrigin(self):
         return ((self.x ** 2) + (self.y ** 2)) ** 0.5
 
+    def __str__(self):
+        return 'Point ({}, {})'.format(self.x, self.y)
+
 
 p = Point(7, 6)
-print(p.getX())
-print(p.getY())
-print(p.distanceFromOrigin())
+# print(p.getX())
+# print(p.getY())
+# print(p.distanceFromOrigin())
+print(p)
+p2 = Point(8, 9)
+print(p2)
