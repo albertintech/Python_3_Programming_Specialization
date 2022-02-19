43e845a55d54d808ff44d1975796beb40d0512c9
Albert Ramos <albertramos.dev@gmail.com>
2022-02-19 06:08:51 -0700

    Add assessment folder

---
+++ Course_4/week1/assessment/1.py
@@ -0,0 +1,1 @@
+# Define a class called Bike that accepts a string and a float as input, and assigns those inputs respectively to two instance variables, color and price. Assign to the variable testOne an instance of Bike whose color is blue and whose price is 89.99. Assign to the variable testTwo an instance of Bike whose color is purple and whose price is 25.0.
